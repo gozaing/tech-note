a65ba39744a825eb9c407b1b816e0f13379e4bd3
gozaing <steganographi@gmail.com>
2017-07-23 22:38:38 +0900

    add sample routing in homestead

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/test', function () {
+    return view('welcome');
+});
 
 Route::get('/', function () {
     return view('welcome');
