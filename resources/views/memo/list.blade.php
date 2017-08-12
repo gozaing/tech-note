e0484c0af3fa719a1f1b6dc4e16281442572a468
gozaing <steganographi@gmail.com>
2017-08-13 01:14:59 +0900

    add sample form

--- resources/views/memo/list.blade.php
+++ resources/views/memo/list.blade.php
@@ -64,6 +64,16 @@
 
     <div id="contents">
         <h2>contents</h2>
+        {!! Form::open(['url' => 'memo']) !!}
+
+        {{ Form::label('title', null, ['class' => 'control-label']) }}
+        {{ Form::text('username') }}
+        {{ Form::label('body', null, ['class' => 'control-label']) }}
+        {{ Form::textarea('email', 'example@gmail.com') }}
+        {{ Form::submit('Click Me!') }}
+
+        {!! Form::close() !!}
+
     </div>
 
     <footer>
