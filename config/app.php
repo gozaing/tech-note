953cd1e72d40ac6bf561d4535cda6387febbf7cd
gozaing <steganographi@gmail.com>
2017-08-13 01:14:51 +0900

    add html form helper

--- config/app.php
+++ config/app.php
@@ -177,7 +177,14 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Form Helper
+         */
+        Collective\Html\HtmlServiceProvider::class,
+
         App\Providers\RepositoryProvider::class,
+
+
 
     ],
 
@@ -227,6 +234,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
