3f8312f0f43c1676517c18311b90ed8dba4b6490
gozaing <steganographi@gmail.com>
2017-08-28 22:14:37 +0900

    change default value

--- resources/views/memo/list.blade.php
+++ resources/views/memo/list.blade.php
@@ -14,10 +14,10 @@
     {!! Form::open(['url' => 'memo']) !!}
 
     {{ Form::label('title', null, ['class' => 'control-label']) }}
-    {{ Form::text('title', 'default title') }}
+    {{ Form::text('title', '') }}
     {{ Form::label('memo', null, ['class' => 'control-label']) }}
-    {{ Form::textarea('memo', 'memo 入力欄') }}
-    {{ Form::submit('Click Me!') }}
+    {{ Form::textarea('memo', '') }}
+    {{ Form::submit('save') }}
 
     {!! Form::close() !!}
 @endsection
